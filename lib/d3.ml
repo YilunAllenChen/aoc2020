217fa4ee1a881a98b06b405a30ebb3e00bbb0791
YilunAllenChen <allenchenyilun1999@gmail.com>
2025-06-19 20:30:28 -0500

    d3

--- lib/d3.ml
+++ lib/d3.ml
@@ -22,18 +22,30 @@ let gather_tree_locations (text : string list) : location list =
   in
   List.mapi cells ~f:acc_in_line |> List.bind ~f:Fn.id
 
-let is_on_slope (xmax : int) (loc : location) : bool =
-  let padding_multiplier = loc.y * 3 / xmax in
+let is_on_slope (xmax : int) (xmult : int) (ymult : int) (loc : location) : bool
+    =
+  let padding_multiplier = loc.y * ymult / xmult / xmax in
   let padded_x = loc.x + (xmax * padding_multiplier) in
-
-  match loc.y with 0 -> false | y -> Int.equal padded_x (y * 3)
+  Int.equal (padded_x * xmult) (loc.y * ymult)
 
 let solution_pt1 (data : string list) : string =
   let xmax = List.hd_exn data |> String.length in
   let all_tree_locs = gather_tree_locations data in
-  let trees_on_slope = all_tree_locs |> List.filter ~f:(is_on_slope xmax) in
+  let trees_on_slope = all_tree_locs |> List.filter ~f:(is_on_slope xmax 1 3) in
 
   (* trees_on_slope |> [%sexp_of: location list] |> Sexp.to_string_hum *)
   trees_on_slope |> List.length |> Int.to_string
 
-let solution_pt2 _ = ""
+let solution_pt2 (data : string list) : string =
+  let xmax = List.hd_exn data |> String.length in
+  let all_tree_locs = gather_tree_locations data in
+
+  let slopes = [ (1, 1); (1, 3); (1, 5); (1, 7); (2, 1) ] in
+  let product =
+    List.fold slopes ~init:1 ~f:(fun acc (xmult, ymult) ->
+        acc
+        * (all_tree_locs
+          |> List.filter ~f:(is_on_slope xmax xmult ymult)
+          |> List.length))
+  in
+  product |> Int.to_string
